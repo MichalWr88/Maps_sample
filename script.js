eeacbc452e6bdf0fd2690a19fb8242b74e252122
MIchal Maleszewski <maleszewski.geo@gmail.com>
2018-02-19 22:08:10 +0100

    first update
    create geojson, html, css, js files

---
+++ script.js
@@ -0,0 +1,136 @@
+const mapInfo = document.getElementById("map-details"),
+    details = document.querySelector("#map-details"),
+    estate = "./DATA/OSIEDLA.geojson",
+    maps = document.getElementById('map'),
+    groups = "DATA/GRUPY.geojson";
+    sourceTmp = document.getElementById("template-details");
+    // temp = Handlebars.compile(sourceTmp);
+
+
+window.addEventListener("load", () => {
+    initMap();
+
+}, false); 
+
+const getCentroids = (obj)=>{
+
+}
+
+const initMap = () => {
+    var position = { lat: 51.1267432, lng: 17.063248 };
+    var map = new google.maps.Map(maps, {
+        zoom: 11,
+        center: position
+    });
+
+    /*LOAD DATA GEOJSON*/
+    map.data.loadGeoJson(groups);
+    map.data.loadGeoJson(estate);
+
+    map.data.setStyle((elem) => {
+        // console.log(elem.getProperty('NAZWAOSIED'));
+
+        if (elem.f.GRUPA == undefined) {
+            // let polyBnds = new google.maps.LatLngBounds(),
+            //     bounds = [];
+            // elem.getGeometry().forEachLatLng(function(path) {
+            //     bounds.push(path);
+            //     polyBnds.extend(path);
+            // });
+            // let area = google.maps.geometry.spherical.computeArea(bounds);
+            //  iW = new google.maps.InfoWindow({
+            //   content: `GRUPA: ${elem.getProperty('GRUPA')}`,
+            //   position: polyBnds.getCenter()
+            // });
+            // let iW = new google.maps.Marker({
+            //     map: map,
+            //     title: `OBREB: ${elem.getProperty('NAZWAOSIED')}`,
+            //     position: polyBnds.getCenter(),
+
+            // });
+
+            // iW.open(map);
+            // let bounds = elem.getGeometry();
+            map.data.addListener('mouseover', function(event) {
+                map.data.revertStyle();
+                map.data.overrideStyle(event.feature, { strokeWeight: 8 });
+                mapInfo.innerHTML = `Obreb ${event.feature.f.NAZWAOSIED}`;
+
+            });
+
+
+            return {
+                fillColor: getRandomColor(),
+                clickable: true,
+                zIndex: 2,
+
+
+            }
+        } else {
+
+            return {
+                fillColor: 'transparent',
+                clickable: false,
+                strokeWeight: 1,
+
+            }
+
+            // elem.addListener('mouseover', function(event) {
+            //     elem.revertStyle();
+            //     // elem.overrideStyle(event.feature, { strokeWeight: 8 });
+            //     // mapInfo.innerHTML = event.feature.f.NAZWAOSIED;
+
+            // });
+            // elem.addListener('mouseout', function(event) {
+            //     // elem.revertStyle();
+            //     elem.overrideStyle(event.feature, { strokeWeight: 1 });
+            //     // mapInfo.innerHTML = event.feature.f.NAZWAOSIED;
+
+            // });
+
+
+        }
+
+
+    });
+
+        map.data.addListener('click', function(event) {
+            getWiki(event.feature.f.NAZWAOSIED);
+        });
+
+
+    //     map.data.addListener('click', function(event) {
+    //         if(event.feature.f.GRUPA !== 1){
+    // console.log(event.feature.f);
+    //         }
+    //     });
+
+    // });
+    // 
+    // 
+    /*GRUPY*/
+    // let grupy = map.data.loadGeoJson('grupa1.geojson');
+    //     grupy.setStyle((elem) => {      
+    //        return {
+    //            fillColor: 'red',
+    //            clickable: true,
+    //        }
+    //    });
+
+
+
+}
+
+
+
+
+/*FUNCTIONS*/
+
+const getRandomColor = ()=>{
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
